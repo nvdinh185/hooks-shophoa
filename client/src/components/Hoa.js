b93a1a8d207301dcec7ecee49a73fc9cee9a316f
nvdinh185 <nvdinh0766777123@gmail.com>
2024-03-11 16:37:33 +0700

    x

--- client/src/components/Hoa.js
+++ client/src/components/Hoa.js
@@ -9,13 +9,13 @@ const Hoa = (props) => {
             </td>
             <td>{props.catName}</td>
             <td align="center">
-                <img src={"images/tmp/" + props.image} className="hoa" />
+                <img src={"images/tmp/" + (props.image || 'nopicture.jpg')} className="hoa" />
             </td>
             <td align="center">
                 <Link to={'/edit/' + props.id}>
                     Sửa <img src="images/pencil.gif" alt="edit" />
                 </Link>
-                <a href="javascript:;" onClick={props.onClickDel}>
+                <a onClick={props.onClickDel}>
                     Xóa{" "}
                     <img
                         src="images/bin.gif"
